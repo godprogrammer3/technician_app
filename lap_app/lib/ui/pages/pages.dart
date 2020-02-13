7bf27e7d2024e18e174d0403d85119c33528f92a
gofprogrammer <game47you@gmail.com>
2020-02-13 12:10:49 +0700

    ui almost finish

--- lap_app/lib/ui/pages/pages.dart
+++ lap_app/lib/ui/pages/pages.dart
@@ -2,3 +2,4 @@ export './verify_otp_page.dart';
 export './request_otp_page.dart';
 export './yourtask.dart';
 export './home_page.dart';
+export './search_result_page.dart';
--- lap_app/lib/ui/pages/search_result_page.dart
+++ lap_app/lib/ui/pages/search_result_page.dart
@@ -1,18 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lap_app/bloc/bloc.dart';
+import 'package:rounded_floating_app_bar/rounded_floating_app_bar.dart';
 
 class SearchResultPage extends StatelessWidget {
   const SearchResultPage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: BlocProvider(
+    return BlocProvider(
         create: (context) => SearchResultBloc(),
         child: SearchResultPageChild(),
-      ),
-    );
+      );
   }
 }
 
@@ -23,17 +22,56 @@ class SearchResultPageChild extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: buildBody(context),
+        child: Padding(
+          padding:EdgeInsets.fromLTRB(0, 10, 0, 0),
+          child:buildBody(context),
+        ),
+        
       ),
     );
   }
 
-  Widget buildBody(BuildContext context){
-    return Column(
-      children: <Widget>[
-        Row(
-          
-        ),  
-    ]);
+  Widget buildBody(BuildContext context) {
+    return  NestedScrollView(
+        headerSliverBuilder: (context, isInnerBoxScroll) {
+          return [
+            RoundedFloatingAppBar(
+              actions: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.clear,size: 30,),
+                  onPressed: () {},
+                ),
+              ],
+              floating: true,
+              snap: true,
+              title: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                 IconButton(
+                   icon:Icon(Icons.keyboard_backspace,size:30,),
+                   onPressed: () {
+
+                   }
+                 ),
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'ค้นหางานใหม่รอดำเนินการ',
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              backgroundColor: Colors.white,
+            ),
+          ];
+        },
+        body: ListView.builder(
+          itemBuilder: (_, i) => ListTile(
+                title: Text("Title $i"),
+              ),
+        ),
+      );
   }
 }
