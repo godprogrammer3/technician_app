f9ed1ef488088bce742c37262be2b0fafe453de5
gofprogrammer <game47you@gmail.com>
2020-02-14 18:28:28 +0700

    add internet permission

--- lap_app/lib/main.dart
+++ lap_app/lib/main.dart
@@ -7,32 +7,14 @@ import 'package:lap_app/bloc/bloc.dart';
 import 'package:lap_app/ui/pages/pages.dart';
 import 'package:lap_app/ui/pages/wait_task.dart';
 import 'dart:async';
-
 import 'data/entities/entities.dart';
-// cil2
 import 'package:lap_app/data/datasources/datasources.dart';
-// cil
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
       .then((_) {
     runApp(new MyApp());
   });
-  // final assetResources = AssetResources();
-  // try{
-  //   final jsonString= await assetResources.loadJsonString('search_result_job.json');
-  //   try{
-  //     final json1 =  json.decode(jsonString[0]);
-  //     print(json1);
-  //   }catch(e){
-  //     print(e);
-  //   }
-    
-    
-  // }catch(e){
-  //   print(e);
-  // }
-
 }
 
 class MyApp extends StatelessWidget {
