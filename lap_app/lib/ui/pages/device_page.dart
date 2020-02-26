75f92f0826eb082cf03a5baf1017eba2ad0b91e1
suurii <turtle4941@gmail.com>
2020-02-26 21:38:10 +0700

    add bottom bar

--- lap_app/lib/ui/pages/device_page.dart
+++ lap_app/lib/ui/pages/device_page.dart
@@ -36,45 +36,63 @@ class DevicePage extends StatelessWidget {
               
             ),
             bottomNavigationBar: BottomAppBar(
-                color: Colors.white,
-                child: Row(
+              color: Colors.white,
+              child: Container(
+                margin: EdgeInsets.all(10),
+                child:Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: <Widget>[
-                    Container(
-                      // margin: EdgeInsets.all(20),
-                      // width: double.infinity,
-                      // height: 50,
-                      child: RaisedButton(
-                        color: const Color(0xFF2FDC96),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: new BorderRadius.circular(8.0),
-                        ),
-                        textColor: Colors.white,
-                        onPressed: () => print('hello'),
-                        child: Container(
-                            margin: EdgeInsets.only(
-                              left: 10,
-                              right: 10,
-                            ),
-                            child: Text(
-                              'รับทำงาน',
-                              style: TextStyle(
-                                  fontFamily: 'supermarket', fontSize: 20),
-                          )),
-                  ),
-                )
+                    ButtonTheme(
+                      
+                      minWidth: MediaQuery.of(context).size.width*0.52,
+                      height:50,
+                      child: FlatButton(
+                        shape: new RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(8.0),
+                            side: BorderSide(color: Colors.green)),
+                        color: Color(0xFAFAFA),
+                        textColor: Colors.green,
+                        padding: EdgeInsets.all(17.0),
+                        onPressed: () {
+                          confirmDialog(context);
+                        },
+                        child: Text(
+                          'เมนูการทำงาน',
+                          style: TextStyle(fontFamily: 'supermarket', fontSize: 20, color: const Color(0xFF46B085)), ),
+                      ),
+                    ),
+                    
+                   
+                    ButtonTheme(
+                      
+                      minWidth: MediaQuery.of(context).size.width*0.38,
+                      height:50,
+                      child: FlatButton(
+                        shape: new RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(8.0),
+                            side: BorderSide(color: Color.fromARGB(255, 47, 220, 150))),
+                        color: Color.fromARGB(255, 47, 220, 150),
+                        textColor: Colors.green,
+                        padding: EdgeInsets.all(17.0),
+                        onPressed: () {
+                          confirmDialog(context);
+                        },
+                        child: Text(
+                          'Console >',
+                          style: TextStyle(fontFamily: 'supermarket', fontSize: 20, color: Colors.white), ),
+                      ),
+                    ),
                   ],
+              )
+              )
+               
 
-
-                )
-                
-                
-                
-                ));
+              ));
   }
   Widget buildBody(BuildContext context) {
-     List<String> nameDevice = <String>['SWITCH1','SWITCH1','SWITCH1'];
-     List<String> modelDevice = <String>['123','456','789'];
-     List<String> typeDevice = <String>['Switch','Lab','Multimeter'];
+     String nameDevice = 'SWITCH1';
+     String modelDevice = '123';
+     String typeDevice = 'Switch';
     return Column(
       children: <Widget>[
         Row(   
@@ -92,11 +110,11 @@ class DevicePage extends StatelessWidget {
                 
                 child: Column(
                   children: <Widget>[
-                    Align(alignment: Alignment.centerLeft,child:  Text('aaaaaaaaaaaaa',style: TextStyle(fontFamily: 'supermarket',fontSize: 18),)),
+                    Align(alignment: Alignment.centerLeft,child:  Text('${nameDevice}',style: TextStyle(fontFamily: 'supermarket',fontSize: 18),)),
                     Container(margin: EdgeInsets.only(top: 6),),
-                    Align(alignment: Alignment.centerLeft,child:  Text('รุ่นอุปกรณ์ : sdsdfsdf ',style: TextStyle(fontFamily: 'supermarket',fontSize: 18)),),
+                    Align(alignment: Alignment.centerLeft,child:  Text('รุ่นอุปกรณ์ : ${modelDevice} ',style: TextStyle(fontFamily: 'supermarket',fontSize: 18)),),
                     Container(margin: EdgeInsets.only(top: 6),),
-                    Align(alignment: Alignment.centerLeft,child:  Text('TYPE : sdfsdfds',style: TextStyle(fontFamily: 'supermarket',fontSize: 18)),),
+                    Align(alignment: Alignment.centerLeft,child:  Text('TYPE : ${typeDevice}',style: TextStyle(fontFamily: 'supermarket',fontSize: 18)),),
                   ],
                 ), ),
             ],
@@ -110,5 +128,81 @@ class DevicePage extends StatelessWidget {
                 
        
             );
+  }
+  void confirmDialog(BuildContext context){
+    var cfDialog = AlertDialog(
+      title: Text("ยืนยันการรับทำงาน",style: TextStyle(fontFamily: 'supermarket', fontSize: 20, color: Colors.black),textAlign: TextAlign.center,),
+      content: Text("ชื่องาน/โปรเจคที่จะรับนะจ้ะ pass ค่ามาให้ด้วย"),
+      actions: <Widget>[
+        Row(
+                  //mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    
+                    ButtonTheme(
+                      
+                      minWidth: MediaQuery.of(context).size.width*0.35,
+                      height:10,
+                      child: FlatButton(
+                        shape: new RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(8.0),
+                            side: BorderSide(color: Colors.red)),
+                        color: Color(0xFAFAFA),
+                        textColor: Colors.red,
+                        padding: EdgeInsets.all(17.0),
+                        onPressed: () {
+                          confirmDialog(context);
+                        },
+                        child: Text(
+                          'ยกเลิก',
+                          style: TextStyle(fontFamily: 'supermarket', fontSize: 18, color:Colors.red), ),
+                      ),
+                    ),
+                    Container(margin:EdgeInsets.only(left:14),),
+                   
+                    ButtonTheme(
+                      
+                      minWidth: MediaQuery.of(context).size.width*0.35,
+                      height:10,
+                      child: FlatButton(
+                        shape: new RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(8.0),
+                            side: BorderSide(color: Colors.green)),
+                        color: Color(0xFAFAFA),
+                        textColor: Colors.green,
+                        padding: EdgeInsets.all(17.0),
+                        onPressed: () {
+                          confirmDialog(context);
+                        },
+                        child: Text(
+                          'ยืนยัน',
+                          style: TextStyle(fontFamily: 'supermarket', fontSize: 18, color: const Color(0xFF46B085)), ),
+                      ),
+                    ),
+                    Container(margin:EdgeInsets.only(left:5),),
+                  ],
+              )
+        // OutlineButton(
+          
+        //   child: Text("ยกเลิก"),
+        //   onPressed: (){
+        //     Navigator.pop(context);
+        //  },),
+        // OutlineButton(
+        //   child: Text("ยืนยัน"),
+        //   onPressed: (){
+        //     print('Yeah ok you got this job by API and going to next page');
+        //     // Navigator.pop(context);
+        //   },)
+
+      ],
+    );
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context){
+        return cfDialog;
+      }
+    );
+
   }
 }
